0bc1122e37647ac8deaeb5c43aca39c3aaa65290
Joey Pinhas <jpinhas97@yahoo.com>
2017-03-06 14:30:43 -0500

    Adds a few projects to the site, and arranges them nicely. Also removes unnecessary files

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 (function() {
-  console.log('Hello!');
+  /*
+
+  TODO: implement a page scrolling feature (scroll one "page" at a time)
+
+  let winHeight = window.height;
+  let winWidth = window.width;
+  let curPage = 0;
+
+  function getViewportDimensions() {
+    winHeight = window.height;
+    winWidth = window.width;
+  }
+
+  function scrollEventListener() {
+    // get current position of scroller on the page
+    // determine the strength of the scroll
+    // calculate the end position of the scroll:
+    // if it's greater than a page height, scrollto the next page
+    // if not, do nothing (maybe display a little gesture?)
+  }
+
+  */
 })();
